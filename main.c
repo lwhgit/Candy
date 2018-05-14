fe8ab3e45365d1b51595fca5c57ac12c975696b0
lwh8762 <lwh8762@naver.com>
2018-05-14 23:17:19 +0900

    Update main.c

--- main.c
+++ main.c
@@ -14,67 +14,67 @@ void showBox();
 
 int main() {
 
-	index = 0;					// 현재 상자 위치
+    index = 0;                  // 현재 상자 위치
 
-	scanf_s("%d %d", &n, &m);
+    scanf_s("%d %d", &n, &m);
 
-	eatDay = m / 2;				// 먹는날
-	giveDay = eatDay + m % 2;	// 받는날
+    eatDay = m / 2;             // 먹는날
+    giveDay = eatDay + m % 2;   // 받는날
 
-	int i;
-	for (i = 0; i < giveDay; i ++) {
-		scanf("%s", eat[i]);
-	}
-	for (i = 0; i < giveDay; i ++) {
-		scanf("%d", &give[i]);
-	}
+    int i;
+    for (i = 0; i < giveDay; i ++) {
+        scanf("%s", eat[i]);
+    }
+    for (i = 0; i < giveDay; i ++) {
+        scanf("%d", &give[i]);
+    }
 
-	
-	int day;
-	for (day = 1; day <= m; day ++) {
-		if (day % 2 == 1) {
-			putCandy(eat[(day - 1) / 2]);
-		} else if (day % 2 == 0) {
-			eatCandy(give[(day - 1) / 2]);
-		}
-	}
+    
+    int day;
+    for (day = 1; day <= m; day ++) {
+        if (day % 2 == 1) {
+            putCandy(eat[(day - 1) / 2]);
+        } else if (day % 2 == 0) {
+            eatCandy(give[(day - 1) / 2]);
+        }
+    }
 
-	showBox();
+    showBox();
 
-	system("pause");
+    system("pause");
 
-	return 0;
+    return 0;
 }
 
 void putCandy(char* c) {
-	int i;
-	for (i = 0; *(c + i) != 0; i ++) {
-		box[index] =  *(c + i);
-		index ++;
+    int i;
+    for (i = 0; *(c + i) != 0; i ++) {
+        box[index] =  *(c + i);
+        index ++;
 
-		if (index >= n) {		// 여긴 안전성때문에 넣은곳
-			index = n - 1;		// 딱히 필요한 코드는 아니지만 혹시몰라 넣은코드.
-		}						//
-	}
+        if (index >= n) {        // 여긴 안전성때문에 넣은곳
+            index = n - 1;       // 딱히 필요한 코드는 아니지만 혹시몰라 넣은코드.
+        }                        //
+    }
 }
 
 void eatCandy(int count) {
-	int i;
-	for (i = 0;i < count;i ++) {
-		box[index] = 0;
-		index --;
+    int i;
+    for (i = 0;i < count;i ++) {
+        box[index] = 0;
+        index --;
 
-		if (index < 0) {		// 여긴 안전성때문에 넣은곳
-			index = 0;			// 딱히 필요한 코드는 아니지만 혹시몰라 넣은코드.
-		}						//
-	}
+        if (index < 0) {        // 여긴 안전성때문에 넣은곳
+            index = 0;          // 딱히 필요한 코드는 아니지만 혹시몰라 넣은코드.
+        }                       //
+    }
 }
 
 void showBox() {
-	int i;
-	for (i = index - 1;i >= 0; i --) {
-		printf("%c", box[i]);
-	}
+    int i;
+    for (i = index - 1;i >= 0; i --) {
+        printf("%c", box[i]);
+    }
 
-	printf("\n");
+    printf("\n");
 }
