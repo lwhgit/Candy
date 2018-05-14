b56eb2cc143b054143b39abe1494e0f439cf2422
lwh8762 <lwh8762@naver.com>
2018-05-14 23:15:56 +0900

    Create main.c

---
+++ main.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+int n, m, giveDay, eatDay, index;
+int give[MAX];
+char box[MAX];
+char eat[MAX][MAX];
+
+void putCandy(char *c);
+void eatCandy(int count);
+void showBox();
+
+int main() {
+
+	index = 0;					// 현재 상자 위치
+
+	scanf_s("%d %d", &n, &m);
+
+	eatDay = m / 2;				// 먹는날
+	giveDay = eatDay + m % 2;	// 받는날
+
+	int i;
+	for (i = 0; i < giveDay; i ++) {
+		scanf("%s", eat[i]);
+	}
+	for (i = 0; i < giveDay; i ++) {
+		scanf("%d", &give[i]);
+	}
+
+	
+	int day;
+	for (day = 1; day <= m; day ++) {
+		if (day % 2 == 1) {
+			putCandy(eat[(day - 1) / 2]);
+		} else if (day % 2 == 0) {
+			eatCandy(give[(day - 1) / 2]);
+		}
+	}
+
+	showBox();
+
+	system("pause");
+
+	return 0;
+}
+
+void putCandy(char* c) {
+	int i;
+	for (i = 0; *(c + i) != 0; i ++) {
+		box[index] =  *(c + i);
+		index ++;
+
+		if (index >= n) {		// 여긴 안전성때문에 넣은곳
+			index = n - 1;		// 딱히 필요한 코드는 아니지만 혹시몰라 넣은코드.
+		}						//
+	}
+}
+
+void eatCandy(int count) {
+	int i;
+	for (i = 0;i < count;i ++) {
+		box[index] = 0;
+		index --;
+
+		if (index < 0) {		// 여긴 안전성때문에 넣은곳
+			index = 0;			// 딱히 필요한 코드는 아니지만 혹시몰라 넣은코드.
+		}						//
+	}
+}
+
+void showBox() {
+	int i;
+	for (i = index - 1;i >= 0; i --) {
+		printf("%c", box[i]);
+	}
+
+	printf("\n");
+}
